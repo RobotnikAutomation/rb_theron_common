4886071a43cb8aa74d3078956fed172538c467c8
Guillem Gari <ggari@robotnik.es>
2023-08-16 16:21:18 +0200

    Changed enterprise

--- rb_theron_description/launch/default.launch.py
+++ rb_theron_description/launch/default.launch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Robotnik Automation S.L.L.
+# Copyright (c) 2023, Robotnik Automation S.L.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
